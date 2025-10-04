88ca41206b8bf17839a6bd1aee419c8559144cf7
pranay-gottimukula <pranaygottimukula@gmail.com>
2025-10-05 00:59:58 +0530

    add directory pages

--- frontend/app/(directory)/public-representatives/page.jsx
+++ frontend/app/(directory)/public-representatives/page.jsx
@@ -1,7 +1,13 @@
 export default function Page(){
     return (
-        <div>
-            Public Representatives
-        </div>
+        <main className="container mx-auto flex min-h-screen flex-col items-center justify-start px-4 py-16">
+            <header className="mb-8 text-left w-10/12">
+                <h1 className="text-4xl font-medium text-primary mb-2">Public Representatives</h1>
+                <hr className="my-4 h-1 bg-primary"/>
+                <p className="italic text-lg text-muted-foreground">
+                  Welcome to the Public Representatives page of our smart city portal. <br />
+                </p>
+            </header>
+        </main>
     );
 }
--- frontend/app/(directory)/std-pin-codes/page.jsx
+++ frontend/app/(directory)/std-pin-codes/page.jsx
@@ -1,7 +1,13 @@
 export default function Page(){
     return (
-        <div>
-            STD & Pin codes
-        </div>
+        <main className="container mx-auto flex min-h-screen flex-col items-center justify-start px-4 py-16">
+            <header className="mb-8 text-left w-10/12">
+        <h1 className="text-4xl font-medium text-primary mb-2">STD & PIN codes</h1>
+        <hr className="my-4 h-1 bg-primary"/>
+        <p className="italic text-lg text-muted-foreground">
+          Welcome to the STD & PIN codes page of our smart city portal. <br />
+        </p>
+      </header>
+        </main>
     );
 }
