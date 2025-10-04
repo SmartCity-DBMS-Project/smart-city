cdf07edc5f4b68280d342de20010fb74ba9e9359
pranay-gottimukula <pranaygottimukula@gmail.com>
2025-10-05 00:59:58 +0530

    add directory pages

--- frontend/app/(directory)/public-utilities/[utility]/page.jsx
+++ frontend/app/(directory)/public-utilities/[utility]/page.jsx
@@ -2,8 +2,14 @@ export default function utilityPage({params}){
     const utility = params.utility;
 
     return (
-        <div>
-            This page contains information of all {utility}
-        </div>
+        <main className="container mx-auto flex min-h-screen flex-col items-center justify-start px-4 py-16">
+            <header className="mb-8 text-left w-10/12">
+                <h1 className="text-4xl font-medium text-primary mb-2">{utility}</h1>
+                <hr className="my-4 h-1 bg-primary"/>
+                <p className="italic text-lg text-muted-foreground">
+                  Welcome to the {utility} page of our smart city portal. <br />
+                </p>
+            </header>
+        </main>
     );
 }
--- frontend/app/(directory)/public-utilities/page.jsx
+++ frontend/app/(directory)/public-utilities/page.jsx
@@ -1,14 +1,44 @@
 import Link from "next/link";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 
 export default function Page(){
+    const utilities = [
+        {name: "Banks", link: "banks"},
+        {name: "Collages & Universities", link: "collages-universities"},
+        {name: "Schools", link: "schools"},
+        {name: "Hospitals", link: "hospitals"},
+        {name: "Postal", link: "postal"}
+    ]
     return (
-        <div>
-            Public Buildings
-            <Link href={`/public-utilities/banks`}>Banks</Link>
-            <Link href={`/public-utilities/collages-universities`}>Collages & Universities</Link>
-            <Link href={`/public-utilities/schools`}>Schools</Link>
-            <Link href={`/public-utilities/hospitals`}>Hospitals</Link>
-            <Link href={`/public-utilities/postal`}>Postal</Link>
-        </div>
+        <main className="container mx-auto flex min-h-screen flex-col items-center justify-start px-4 py-16">
+            <header className="mb-8 text-left w-10/12">
+                <h1 className="text-4xl font-medium text-primary mb-2">Public Utilities</h1>
+                <hr className="my-4 h-1 bg-primary"/>
+                <p className="italic text-lg text-muted-foreground">
+                  Welcome to the Public Utilities page of our smart city portal. <br />
+                </p>
+            </header>
+            {/* Cards Row */}
+            <div className="grid w-10/12 bg-card p-4 grid-cols-5 gap-6">
+              {utilities.map((utils) => (
+                <Link
+                  key={utils.link}
+                  href={`/public-utilities/${utils.link}`}
+                  className="flex-1"
+                >
+                  <Card
+                    className="bg-background text-acc-blue rounded-none aspect-square border-0 shadow-none flex items-center justify-center hover:bg-acc-blue hover:text-background transition-colors duration-200"
+                  >
+                    <CardHeader className="p-0 flex items-center justify-center">
+                      <CardTitle className="text-center text-xl font-medium">
+                        {utils.name}
+                      </CardTitle>
+                    </CardHeader>
+                  </Card>
+                </Link>
+              ))}
+            </div>
+        </main>
     );
 }
