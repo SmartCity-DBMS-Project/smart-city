53acb07a6148ede72a3ca56ab902c332395c92ad
pranay-gottimukula <pranaygottimukula@gmail.com>
2025-10-06 00:16:57 +0530

    add loginform

--- frontend/app/login/layout.jsx
+++ frontend/app/login/layout.jsx
@@ -1,6 +1,6 @@
 export default function LoginLayout({ children }) {
   return (
-    <div className="flex items-center justify-center p-12 m-auto mt-5 bg-[#6AA795] w-[50vw] border rounded-lg ">
+    <div>
       {children}
     </div>
   )
