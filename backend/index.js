44474f6c554b6106565094eacfd4cc345ba3eec2
pranay-gottimukula <pranaygottimukula@gmail.com>
2025-10-13 01:56:24 +0530

    add buildings route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
-import express from "express";
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const buildingRouter = require('./routes/buildings');
+const { json } = require('stream/consumers');
 
 const app = express();
 const prisma = new PrismaClient();
@@ -9,6 +10,6 @@ const PORT = process.env.PORT || 8000
 
 app.use(express.json());
 
-
+app.use("/api/buildings", buildingRouter);
 
 app.listen(PORT, () => console.log("Server running on port 8000"));
